c1637f2104bf031cf4c016e5d3cfff3b5de78b4b
Carlo Bernardini <bernardini@textkernel.nl>
2016-12-29 16:07:58 +0100

    Extended docs; Collapse code blocks

--- docs/docs.js
+++ docs/docs.js
@@ -7,4 +7,14 @@ $(function(){
         toggle.toggleClass('collapsed', !state);
         $('body').toggleClass('expanded', state);
     });
+    $('code').each(function(){
+        var code = $(this);
+        if (code.outerHeight() > 200) {
+            $(this).addClass('collapsed')
+                .bind('click', function(e){
+                    var isCollapsed = $(this).is('.collapsed');
+                    $(this).toggleClass('collapsed', !isCollapsed);
+                });
+        }
+    });
 });

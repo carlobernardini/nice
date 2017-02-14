16ec013d22e9523edc94b3fe8ff18551a1ac7aa4
Carlo Bernardini <bernardini@textkernel.nl>
2017-02-14 13:25:41 +0100

    Website: Updated code toggle button; Added 'copy' button

--- docs/docs.js
+++ docs/docs.js
@@ -1,3 +1,18 @@
+var ClipboardHelper = {
+    copyElement: function ($element)
+    {
+       this.copyText($element.text())
+    },
+    copyText:function(text) // Linebreaks with \n
+    {
+        var $tempInput =  $("<textarea>");
+        $("body").append($tempInput);
+        $tempInput.val(text).select();
+        document.execCommand("copy");
+        $tempInput.remove();
+    }
+};
+
 $(function(){
     $('.nice-header .nice-header-toggle').bind('click', function(e){
         var toggle = $(this),
@@ -7,14 +22,21 @@ $(function(){
         toggle.toggleClass('collapsed', !state);
         $('body').toggleClass('expanded', state);
     });
-    $('code.docs').each(function(){
-        var code = $(this);
+    $('.docs-code-wrapper').each(function(){
+        var code = $(this).find('code.docs'),
+            toggle = $(this).find('.toggle'),
+            copy = $(this).find('.copy');
         if (code.outerHeight() > 200) {
-            $(this).addClass('collapsed')
-                .bind('click', function(e){
-                    var isCollapsed = $(this).is('.collapsed');
-                    $(this).toggleClass('collapsed', !isCollapsed);
-                });
+            code.addClass('collapsed');
+            toggle.bind('click', function(e){
+                var isCollapsed = code.is('.collapsed');
+                code.toggleClass('collapsed', !isCollapsed);
+                return false;
+            });
+            copy.bind('click', function(e){
+                ClipboardHelper.copyElement(code);
+                return false;
+            });
         }
     });
 });

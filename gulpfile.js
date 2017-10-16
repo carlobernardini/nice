ffbe99df1ffc98275b7d93897e4c148c05e2c00b
Carlo Bernardini <bernardini@textkernel.nl>
2017-10-16 12:22:18 +0200

    Updated included scss paths

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var     gulp = require('gulp'),
 
 var scssIncludes = [
     './node_modules/font-awesome/scss',
-    './node_modules/bourbon/app/assets/stylesheets',
     './node_modules/bourbon-neat/app/assets/stylesheets'
 ];
 
